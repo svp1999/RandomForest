83001f4e8ddbaa78978a7c7760991e50fdcb9f52
svp1999 <78013135+svp1999@users.noreply.github.com>
2021-06-22 15:12:34 +0530

    Add files via upload

---
+++ RandomForest.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+
+pip install tensorflow
+
+
+# In[4]:
+
+
+# Importing all necessary libraries
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+img_width, img_height = 224, 224
+
+
+# In[32]:
+
+
+import os
+train_data_dir = "C:/Users/sds/Downloads/v_data/v_data/train"
+validation_data_dir = "C:/Users/sds/Downloads/v_data/v_data/test"
+nb_train_samples =400
+nb_validation_samples = 100
+epochs = 10
+batch_size = 16
+
+
+# In[33]:
+
+
+if K.image_data_format() == 'channels_first':
+	input_shape = (3, img_width, img_height)
+else:
+	input_shape = (img_width, img_height, 3)
+
+
+# In[34]:
+
+
+model = Sequential()
+model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+# In[35]:
+
+
+model.compile(loss='binary_crossentropy',
+			optimizer='rmsprop',
+			metrics=['accuracy'])
+
+
+# In[36]:
+
+
+train_datagen = ImageDataGenerator(
+	rescale=1. / 255,
+	shear_range=0.2,
+	zoom_range=0.2,
+	horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+	train_data_dir,
+	target_size=(img_width, img_height),
+	batch_size=batch_size,
+	class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+	validation_data_dir,
+	target_size=(img_width, img_height),
+	batch_size=batch_size,
+	class_mode='binary')
+
+model.fit_generator(
+	train_generator,
+	steps_per_epoch=nb_train_samples // batch_size,
+	epochs=epochs,
+	validation_data=validation_generator,
+	validation_steps=nb_validation_samples // batch_size)
+
+
+# In[37]:
+
+
+model.save_weights('model_saved.h5')
+
+
+# In[ ]:
+
+
+
+
